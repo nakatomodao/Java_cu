b1b1788165fad7bbe6e6406a3ddf1ddf5de8efb4
nakada <nakada@localhost>
2023-11-12 00:15:31 +0900

    1.3-1 Java導入

---
+++ HelloWorld_NakadaTomohiro/src/sample/SelfIntroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "山田 花子";
+		int age = 25;
+		String hobby = "映画鑑賞";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
